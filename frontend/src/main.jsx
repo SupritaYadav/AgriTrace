7dc7e66a41eba0aa54bdc264acf44efe6352b77a
ksuvii21 <k21ritikasuvi2106@gmail.com>
2026-09-10 22:29:11 +0530

    Update basic implementation of the frontend.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+import { AuthProvider } from "./context/AuthContext";
+
+import "./styles/variables.css";
+import "./styles/globals.css";
+import "./styles/layout.css";
+import "./styles/components.css";
+import "./styles/pages.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
